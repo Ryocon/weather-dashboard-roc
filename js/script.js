89fbd8733addbb0c1741899e6fc4c83f934025b2
roconn <roconn25@gmail.com>
2022-01-24 19:49:56 +0000

    Implementing basic html + start of js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var APIkey = "840819e30adace1a99c607cc7e47419d"
+
+var cityNameEl = document.getElementById('city-name')
+
+var cityQuery = "api.openweathermap.org/data/2.5/weather?q=" + {cityNameEl} + "&appid=" + {APIkey}

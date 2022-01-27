f1e188fc0b52f771d2265d7d3542ec61d05f85ad
roconn <roconn25@gmail.com>
2022-01-27 21:02:38 +0000

    To do list added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-var APIkey = "840819e30adace1a99c607cc7e47419d"
+var weatherAPIkey = "840819e30adace1a99c607cc7e47419d"
 
 var cityNameEl = document.getElementById('city-name')
 
-var cityQuery = "api.openweathermap.org/data/2.5/weather?q=" + {cityNameEl} + "&appid=" + {APIkey}
+
+var cityInput = document.getElementById('city-input').value
+
+
+
+
+
+// url query to search a city using API key
+var cityQuery = "api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + weatherAPIkey
+
+
+fetch(cityQuery)
+
+// to do
+// unit conversion
+// 5 day forecast
+// pull data from api call

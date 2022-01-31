24e91b63d2188f7b87efcd29fc07bb279d37c4ea
roconn <roconn25@gmail.com>
2022-01-31 21:44:22 +0000

    Updates to API functionality

--- js/script.js
+++ js/script.js
@@ -2,18 +2,45 @@ var weatherAPIkey = "840819e30adace1a99c607cc7e47419d"
 
 var cityNameEl = document.getElementById('city-name')
 
+var cityInput = document.getElementById('city-input')
 
-var cityInput = document.getElementById('city-input').value
+var searchBtnEl = document.getElementById("search-btn")
 
 
 
-
+var testQuery = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=840819e30adace1a99c607cc7e47419d"
 
 // url query to search a city using API key
-var cityQuery = "api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + weatherAPIkey
+// var cityQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&appid=" + weatherAPIkey
 
 
-fetch(cityQuery)
+
+
+searchBtnEl.addEventListener('click', function() {
+
+var myKey = "840819e30adace1a99c607cc7e47419d"
+// API works when key is set locally not globally
+var cityQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&appid=" + myKey
+
+    if (cityInput.value === null) {
+        return
+    }
+    else {
+        fetch(cityQuery)
+        .then(response => response.json())
+  .then(data => console.log(data))
+    }
+}
+
+
+)
+
+
+
+// MAKE THE KEY INTO IT'S OWN FUNCTION????
+
+
+// fetch(cityQuery)
 
 // to do
 // unit conversion

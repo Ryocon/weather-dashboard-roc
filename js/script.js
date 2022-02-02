74e7783145d9169d5183ac29e828abd00ea04769
roconn <roconn25@gmail.com>
2022-02-02 21:46:01 +0000

    Working API pull

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 var weatherAPIkey = "840819e30adace1a99c607cc7e47419d"
 
-var cityNameEl = document.getElementById('city-name')
+var cityNameEl = document.getElementById('city-name-main')
+
+var dateMainEl = document.getElementById('date-main')
+var tempMainEl = document.getElementById('temp-main')
+var windMainEl = document.getElementById('wind-main')
+var humidityMainEl = document.getElementById('humidity-main')
+var UVMainEl = document.getElementById('uv-index-main')
+
+
+
+
+
 
 var cityInput = document.getElementById('city-input')
 
 var searchBtnEl = document.getElementById("search-btn")
 
+
+
+
+
+
+// ADD MOMENT JS!!!!
 
 
 var testQuery = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=840819e30adace1a99c607cc7e47419d"
@@ -20,22 +37,67 @@ searchBtnEl.addEventListener('click', function() {
 
 var myKey = "840819e30adace1a99c607cc7e47419d"
 // API works when key is set locally not globally
-var cityQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&appid=" + myKey
+var cityQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&units=metric&appid=" + myKey
 
     if (cityInput.value === null) {
         return
     }
     else {
+        
         fetch(cityQuery)
         .then(response => response.json())
-  .then(data => console.log(data))
+        .then(function (data){
+
+            cityNameEl.innerHTML = cityInput.value
+
+            var tempData = data.main.temp
+          
+            tempMainEl.innerText = "Temp: " + tempData + " Celsius"
+
+            var windData = data.wind.speed
+
+            windMainEl.innerText = "Wind Speed: " + windData + " m/s"
+
+            var humidData = data.main.humidity
+
+            humidityMainEl.innerText = "Humidity: " + humidData + "%"
+
+            // var uvData = 
+
+        }) 
+
+
+
+        }
+        
     }
-}
 
 
+
+// dateMainEl.innerHTML = data. moment needed !!!
+
+
+
+
+// end of if statament 
+
+// end of event function
 )
 
 
+
+
+
+// function fetchData() {
+//     var myKey = "840819e30adace1a99c607cc7e47419d"
+    
+//     var cityQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&appid=" + myKey
+    
+//     fetch(cityQuery)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+    
+// }
 
 // MAKE THE KEY INTO IT'S OWN FUNCTION????
 

1c3a2f3fc710b631062fe16ba11465f981b8e374
roconn <roconn25@gmail.com>
2022-02-05 19:58:23 +0000

    daily function updates

--- js/script2.js
+++ js/script2.js
@@ -11,7 +11,7 @@ var weatherBoxSmallEl = document.getElementById('weather-box-small')
 
 function getCity(location){
 
-    // locaton = document.getElementById('city-input').value;
+    // to find city to search
     location = document.getElementById("city-input").value
 
     fetch(
@@ -31,6 +31,7 @@ function getCity(location){
         var windData = data.wind.speed
         var humidData = data.main.humidity
         // UV Index Req !!!
+        
 
         //get image varaible
         let icon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
@@ -46,13 +47,15 @@ function getCity(location){
         let humidEl = document.createElement('p')
         //  let uvEl
         
+        
         //append to card
 
         // card.append(mainCard)
-        cityEl.textContent = submitEl.value
+        cityEl.textContent = location
         tempEl.textContent = 'Current Temperature: ' + tempData + 'Celsius'
         windEl.textContent = 'Wind Speed: ' + windData + 'm/s'
         humidEl.textContent = 'Humidity: ' + humidData + '%'
+        
 
         cardIcon.setAttribute("src", icon)
         cardIcon.setAttribute('alt', iconDes)
@@ -72,7 +75,7 @@ function getCity(location){
         let lon = data.coord.lon
 
 
-        // getDailyForecast(lat, lon)
+        getDailyForecast(lat, lon)
 
 
 
@@ -99,6 +102,43 @@ function getCity(location){
 // this will be for create ammend append of cards
 function createForecastCards(forecast){
 
+    // var dailyIcon = data.daily.weather.icon
+    // var dailyIconDes = data.daily.weather.description
+    // var dailyTempData = data.daily.temp
+    // var dailyWindData = data.daily.wind_speed
+    // var dailyHumidData = data.daily.humidity
+    // var dailyUvIndex = data.daily.uvi
+
+    var dailyIcon = forecast.daily.weather.icon
+    var dailyIconDes = forecast.daily.weather.description
+    var dailyTempData = forecast.daily.temp
+    var dailyWindData = forecast.daily.wind_speed
+    var dailyHumidData = forecast.daily.humidity
+    var dailyUvIndexData = forecast.daily.uvi
+
+    let dailyCard = document.createElement('div')
+    let dailyCardIcon = document.createElement('img')
+    let dailytTempEl = document.createElement('p')
+    let dailyWindEl = document.createElement('p')
+    let dailyHumidEl = document.createElement('p')
+    let dailyUvEl = document.createElement('p')
+
+    dailyCardIcon.setAttribute("src", dailyIcon)
+    dailyCardIcon.setAttribute('alt', dailyIconDes)
+    dailytTempEl.textContent = 'Temperature: ' + dailyTempData + 'Celsius'
+    dailyWindEl.textContent = 'Wind Speed: ' + dailyWindData + 'm/s'
+    dailyHumidEl.textContent = 'Humidity: ' + dailyHumidData + '%'
+    dailyUvEl.textContent = 'UV Index: ' + dailyUvIndexData
+
+    
+
+
+    var uvData = forecast.daily.uvi[0]
+    let uvEl = document.createElement('p')
+    uvEl = 'UV Index: ' + uvData
+    mainCard.append(uvEl)
+
+
 }
 
 
@@ -108,18 +148,18 @@ function createForecastCards(forecast){
 function getDailyForecast(lat, lon){
 
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=${weatherAPIkey}`
-    ).then(res.json)
-    .then(function (data){
-        console.log('forecast data: ', data)
-
-
+    ).then (function (response) {
+        return response.json()
+    })
+    .then(function (forecast){
+        console.log('forecast data: ', forecast)
 
         //append to dom
         weatherBoxSmallEl.innerHTML = ""
     
-            for(var i = 1; i < data.daily.length-2; i++ ){
+            for(var i = 1; i < forecast.daily.length-2; i++ ){
 
-                createForecastCards(data.daily[i])
+                createForecastCards(forecast.daily[i])
             }
 
 

45887c99bcb1ce99355cf4023bb000d4c00db8f3
roconn <roconn25@gmail.com>
2022-02-06 20:24:50 +0000

    Function seperation for main and daily cards

--- js/script2.js
+++ js/script2.js
@@ -9,8 +9,8 @@ var mainCard = document.getElementById('main-weather')
 var weatherBoxSmallEl = document.getElementById('weather-box-small')
 
 
-function getCity(location){
 
+function getCity(location){
     // to find city to search
     location = document.getElementById("city-input").value
 
@@ -24,49 +24,49 @@ function getCity(location){
 
         console.log("weather data: ", data);
 
-        //current weather card
+        // //current weather card
 
-        //current weather variables 
-        var tempData = data.main.temp
-        var windData = data.wind.speed
-        var humidData = data.main.humidity
-        // UV Index Req !!!
+        // //current weather variables 
+        // var tempData = data.main.temp
+        // var windData = data.wind.speed
+        // var humidData = data.main.humidity
+        // // UV Index Req !!!
         
 
-        //get image varaible
-        let icon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
-        let iconDes = data.weather[0].description
+        // //get image varaible
+        // let icon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+        // let iconDes = data.weather[0].description
 
 
-        //2  creat our dom elements
-        let card = document.createElement('div')
-        let cardIcon = document.createElement('img')
-        let cityEl = document.createElement('h2')
-        let tempEl = document.createElement('p')
-        let windEl = document.createElement('p')
-        let humidEl = document.createElement('p')
-        //  let uvEl
+        // //2  creat our dom elements
+        // let card = document.createElement('div')
+        // let cardIcon = document.createElement('img')
+        // let cityEl = document.createElement('h2')
+        // let tempEl = document.createElement('p')
+        // let windEl = document.createElement('p')
+        // let humidEl = document.createElement('p')
+        // //  let uvEl
         
         
-        //append to card
+        // //append to card
 
-        // card.append(mainCard)
-        cityEl.textContent = location
-        tempEl.textContent = 'Current Temperature: ' + tempData + ' Celsius'
-        windEl.textContent = 'Wind Speed: ' + windData + ' m/s'
-        humidEl.textContent = 'Humidity: ' + humidData + '%'
+        // // card.append(mainCard)
+        // cityEl.textContent = location
+        // tempEl.textContent = 'Current Temperature: ' + tempData + ' Celsius'
+        // windEl.textContent = 'Wind Speed: ' + windData + ' m/s'
+        // humidEl.textContent = 'Humidity: ' + humidData + '%'
         
 
-        cardIcon.setAttribute("src", icon)
-        cardIcon.setAttribute('alt', iconDes)
+        // cardIcon.setAttribute("src", icon)
+        // cardIcon.setAttribute('alt', iconDes)
 
-        // needs UV !!!
-        card.append(cardIcon, cityEl, tempEl, windEl, humidEl)
+        // // needs UV !!!
+        // card.append(cardIcon, cityEl, tempEl, windEl, humidEl)
 
 
-        // append to html
-        mainCard.innerHTML = ""
-        mainCard.append(card)
+        // // append to html
+        // mainCard.innerHTML = ""
+        // mainCard.append(card)
 
 
         //start weather api
@@ -77,88 +77,32 @@ function getCity(location){
 
         getDailyForecast(lat, lon)
 
-
-
-
-
-
-
-
-
     }
 
     ).catch(function (err){
         console.log(err)
     })
 
-
-
-
-
-
-
-}
-
-// this will be for create ammend append of cards
-function createForecastCards(daily){
-
-
-    var dailyIcon = daily.weather.icon
-    var dailyIconDes = daily.weather.description
-    var dailyTempData = daily.temp
-    var dailyWindData = daily.wind_speed
-    var dailyHumidData = daily.humidity
-    var dailyUvIndexData = daily.uvi
-
-    let dailyCard = document.createElement('div')
-    let dailyCardIcon = document.createElement('img')
-    let dailytTempEl = document.createElement('p')
-    let dailyWindEl = document.createElement('p')
-    let dailyHumidEl = document.createElement('p')
-    let dailyUvEl = document.createElement('p')
-
-    dailyCardIcon.setAttribute("src", dailyIcon)
-    dailyCardIcon.setAttribute('alt', dailyIconDes)
-    dailytTempEl.textContent = 'Temperature: ' + dailyTempData + ' Celsius'
-    dailyWindEl.textContent = 'Wind Speed: ' + dailyWindData + ' m/s'
-    dailyHumidEl.textContent = 'Humidity: ' + dailyHumidData + '%'
-    dailyUvEl.textContent = 'UV Index: ' + dailyUvIndexData
-
-    
-
-    // UV Index for main card
-    var uvData = daily.uvi[0]
-    let uvEl = document.createElement('p')
-    uvEl = 'UV Index: ' + uvData
-    // this doesn't work yet
-    card.append(uvEl)
-
-    // neither does this
-    dailyCard.append(dailyCardIcon, dailytTempEl, dailyWindEl, dailyHumidEl, dailyUvEl)
-    weatherBoxSmallEl.append(dailyCard)
-
-
 }
 
 
-
-
-
+// working from lat and lon
 function getDailyForecast(lat, lon){
 
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=${weatherAPIkey}`
     ).then (function (response) {
         return response.json()
     })
-    .then(function (daily){
-        console.log('forecast data: ', daily)
+    .then(function (data){
+        console.log('forecast data: ', data)
 
         //append to dom
         weatherBoxSmallEl.innerHTML = ""
-    
-            for(var i = 1; i < daily.length-2; i++ ){
 
-                createForecastCards(daily[i])
+            let dailyForecasts = data.daily
+            for(var i = 1; i < dailyForecasts.length-2; i++) {
+
+                createForecastCards(dailyForecasts[i])
             }
 
 
@@ -166,6 +110,99 @@ function getDailyForecast(lat, lon){
     )
 
 }
+
+// IS THE URLQUERY THE ISSUE ???
+function getHourlyForecast(lat, lon) {      
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=${weatherAPIkey}`
+    ).then (function (response) {
+        return response.json()
+    })
+    .then(function (data){
+        console.log('hourly data: ', data)
+
+        mainCard.innerHTML = ""
+        let hourlyData = data.hourly
+        createMainCard(hourlyData)
+        }
+
+    )
+
+} 
+
+
+
+// creating main card
+function createMainCard(hourlyData) {
+
+    var mainIcon = hourlyData.weather.icon
+    var mainIconDes = hourlyData.weather.description
+    var mainTime = hourlyData.dt
+    var mainTempData = hourlyData.daily.temp
+    var mainWindData = hourlyData.daily.wind_speed
+    var mainHumidData = hourlyData.daily.humidity
+    var mainUvIndexData = hourlyData.daily.uvi
+
+
+    let mainCardEl = document.createElement('div')
+    let mainCardIcon = document.createElement('img')
+    let mainTimeEl = document.createElement('p')
+    let mainTempEl = document.createElement('p')
+    let mainWindEl = document.createElement('p')
+    let mainHumidEl = document.createElement('p')
+    let mainUvEl = document.createElement('p')
+
+
+    mainCardIcon.setAttribute("src", mainIcon)
+    mainCardIcon.setAttribute('alt', mainIconDes)
+    mainTimeEl.textContent = mainTime
+    mainTempEl.textContent = 'Temperature: ' + mainTempData + ' Celsius'
+    mainWindEl.textContent = 'Wind Speed: ' + mainWindData + ' m/s'
+    mainHumidEl.textContent = 'Humidity: ' + mainHumidData + '%'
+    mainUvEl = 'UV Index: ' + mainUvIndexData
+     
+    
+    // this doesn't work yet
+    mainCard.append(mainCardIcon, mainTimeEl, mainTempEl, mainWindEl, mainHumidEl, mainUvEl)
+    mainCardEl.append(mainCard)
+}
+
+
+// this will be for create ammend append of cards
+function createForecastCards(dailyForecasts){
+
+
+    var dailyIcon = dailyForecasts.weather.icon
+    var dailyIconDes = dailyForecasts.weather.description
+    var dailyTime = new Date(dailyForecasts.dt * 1000)
+    var dailyTimeConv = dailyTime.toDateString()
+    var dailyTempData = dailyForecasts.temp
+    var dailyWindData = dailyForecasts.wind_speed
+    var dailyHumidData = dailyForecasts.humidity
+    var dailyUvIndexData = dailyForecasts.uvi
+
+    let dailyCard = document.createElement('div')
+    let dailyCardIcon = document.createElement('img')
+    let dailyTimeEl = document.createElement('p')
+    let dailytTempEl = document.createElement('p')
+    let dailyWindEl = document.createElement('p')
+    let dailyHumidEl = document.createElement('p')
+    let dailyUvEl = document.createElement('p')
+
+    dailyCardIcon.setAttribute("src", dailyIcon)
+    dailyCardIcon.setAttribute('alt', dailyIconDes)
+    dailyTimeEl.textContent = dailyTimeConv
+    dailytTempEl.textContent = 'Temperature: ' + dailyTempData + ' Celsius'
+    dailyWindEl.textContent = 'Wind Speed: ' + dailyWindData + ' m/s'
+    dailyHumidEl.textContent = 'Humidity: ' + dailyHumidData + '%'
+    dailyUvEl.textContent = 'UV Index: ' + dailyUvIndexData
+
+    
+    dailyCard.append(dailyCardIcon, dailyTime, dailytTempEl, dailyWindEl, dailyHumidEl, dailyUvEl)
+    weatherBoxSmallEl.append(dailyCard)
+
+
+}
+
 
 
 submitBtn.addEventListener('click', getCity)

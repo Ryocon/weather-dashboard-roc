144185a06a7972f605fa9bf76132e35798b4a29f
roconn <roconn25@gmail.com>
2022-02-14 21:39:27 +0000

    Comments added, working from 1 script

--- js/script.js
+++ js/script.js
@@ -1,110 +1,275 @@
-var weatherAPIkey = "840819e30adace1a99c607cc7e47419d"
+// API key for Openweather
+var weatherAPIkey = "840819e30adace1a99c607cc7e47419d";
 
-var cityNameEl = document.getElementById('city-name-main')
+// originally for to hold the value of the user input but seems to cause issues if removed so it is currently has no usage
+var locationEl = document.getElementById("city-input");
 
-var dateMainEl = document.getElementById('date-main')
-var tempMainEl = document.getElementById('temp-main')
-var windMainEl = document.getElementById('wind-main')
-var humidityMainEl = document.getElementById('humidity-main')
-var UVMainEl = document.getElementById('uv-index-main')
+// variable for the submit button
+var submitBtn = document.getElementById("search-btn");
 
+// variable to append the current/main weather card
+var mainCardEl = document.getElementById("main-weather");
 
+// variable to append forecast cards
+var weatherBoxSmallEl = document.getElementById("weather-box-small");
 
+// variable to append search history
+var searchHistory = document.getElementById("search-history");
 
+// variable for the previous search history button
+var prevBtn = document.getElementById("prev-btn");
 
+// function to set the key and value pair for local storage
+function searchHistorySet(location) {
+  location = document.getElementById("city-input").value;
 
-var cityInput = document.getElementById('city-input')
+  localStorage.setItem("prev-city", location);
+}
 
-var searchBtnEl = document.getElementById("search-btn")
+// function that retreives data from the local storage using the prev-city as the key
+function searchHistoryGet() {
+  return localStorage.getItem("prev-city");
+}
 
+// function that appends local storage data to the page
+// if statement is in place to make sure a blank button is not appened to the page if no data is stored
+function searchHistoryAppend() {
+  prevSearch = searchHistoryGet();
 
+  if (prevSearch !== null) {
+    prevBtn = document.createElement("button");
+    prevBtn.removeAttribute("class", "hide");
+    prevBtn.setAttribute(
+      "class",
+      "p-2 mb-2 btn btn-secondary btn-outline-dark text-light"
+    );
 
+    // sets the text of the button to the name of the previously searched location
+    prevBtn.textContent = prevSearch;
 
+    searchHistory.append(prevBtn);
 
+    // return
+  } else {
+    console.log("Nothing searched");
+  }
 
-// ADD MOMENT JS!!!!
+  // event listener is nested as prevBtn is not a global variable and causes errors if not data is in local storage
+  prevBtn.addEventListener("click", function () {
+    var prevSearchLocation = localStorage.getItem("prev-city");
 
+    console.log(prevSearchLocation);
 
-var testQuery = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=840819e30adace1a99c607cc7e47419d"
+    prevSearchCity(location);
+  });
+}
 
-// url query to search a city using API key
-// var cityQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&appid=" + weatherAPIkey
+// function call to append any previous searches to the page
+searchHistoryAppend();
 
+// function that runs the Openweather API using the previous search location
+function prevSearchCity(prevSearchLocation) {
+  // to find city to search
+  var prevSearchLocation = localStorage.getItem("prev-city");
 
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${prevSearchLocation}&units=metric&appid=${weatherAPIkey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("weather data: ", data);
 
+      let lat = data.coord.lat;
 
-searchBtnEl.addEventListener('click', function() {
+      let lon = data.coord.lon;
 
-var myKey = "840819e30adace1a99c607cc7e47419d"
-// API works when key is set locally not globally
-var cityQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&units=metric&appid=" + myKey
+      getDailyForecast(lat, lon);
+      getCurrentForecast(lat, lon);
+      searchHistorySet(location);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
 
-    if (cityInput.value === null) {
-        return
-    }
-    else {
-        
-        fetch(cityQuery)
-        .then(response => response.json())
-        .then(function (data){
+// function that takes user input to get the city searched location data
+// this is then parsed to the onecall API to retrieve the data
+function getCity(location) {
+  // to find city to search
+  location = document.getElementById("city-input").value;
 
-            cityNameEl.innerHTML = cityInput.value
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${weatherAPIkey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("weather data: ", data);
 
-            var tempData = data.main.temp
-          
-            tempMainEl.innerText = "Temp: " + tempData + " Celsius"
+      let lat = data.coord.lat;
 
-            var windData = data.wind.speed
+      let lon = data.coord.lon;
 
-            windMainEl.innerText = "Wind Speed: " + windData + " m/s"
+      getDailyForecast(lat, lon);
+      getCurrentForecast(lat, lon);
+      searchHistorySet(location);
+      searchHistoryAppend(location);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
 
-            var humidData = data.main.humidity
+// using the lat and lon data from the first city search the data is then fetched and appened to the page
+function getCurrentForecast(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${weatherAPIkey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("Current data: ", data);
 
-            humidityMainEl.innerText = "Humidity: " + humidData + "%"
+      mainCardEl.innerHTML = "";
+      let currentData = data.current;
+      createMainCard(currentData);
+    });
+}
 
-            // var uvData = 
+// creating main card
+function createMainCard(currentData) {
+  var mainIcon = `https://openweathermap.org/img/w/${currentData.weather[0].icon}.png`;
+  var mainIconDes = currentData.weather[0].description;
+  var mainTime = new Date(currentData.dt * 1000);
+  var mainTimeConv = mainTime.toDateString();
+  var mainTempData = currentData.temp;
+  var mainWindData = currentData.wind_speed;
+  var mainHumidData = currentData.humidity;
+  var mainUvIndexData = currentData.uvi;
 
-        }) 
+  let mainCard = document.createElement("div");
+  let mainCardDesc = document.createElement("h3");
+  let mainCardIcon = document.createElement("img");
+  let mainTimeEl = document.createElement("p");
+  let mainTempEl = document.createElement("p");
+  let mainWindEl = document.createElement("p");
+  let mainHumidEl = document.createElement("p");
+  let mainUvEl = document.createElement("p");
 
+  //   if statement to display the appropriate colour to the UV Index
+  // NOTE the UV Index on the current.uvi seems to note display despite being listed as an option on the onecall API
+  if (mainUvIndexData <= 2) {
+    mainUvEl.setAttribute("class", "uvGreen");
+  } else if (mainUvIndexData > 2 && mainUvIndexData <= 5) {
+    mainUvEl.setAttribute("class", "uvYellow");
+  } else if (mainUvIndexData > 5 && mainUvIndexData <= 7) {
+    mainUvEl.setAttribute("class", "uvOrange");
+  } else {
+    mainUvEl.setAttribute("class", "uvRed");
+  }
 
+  mainCardDesc.textContent = "Current Weather:";
+  mainCardIcon.setAttribute("src", mainIcon);
+  mainCardIcon.setAttribute("alt", mainIconDes);
+  mainTimeEl.textContent = mainTimeConv;
+  mainTempEl.textContent = "Temperature: " + mainTempData + " Celsius";
+  mainWindEl.textContent = "Wind Speed: " + mainWindData + " m/s";
+  mainHumidEl.textContent = "Humidity: " + mainHumidData + "%";
+  mainUvEl = "UV Index: " + mainUvIndexData;
 
-        }
-        
-    }
+  mainCard.setAttribute("class", "bg-primary bg-gradient p-2 m-2 rounded");
 
+  // this doesn't work yet
+  mainCard.append(
+    mainCardDesc,
+    mainCardIcon,
+    mainTimeEl,
+    mainTempEl,
+    mainWindEl,
+    mainHumidEl,
+    mainUvEl
+  );
+  mainCardEl.append(mainCard);
+}
 
+// function to set the forecast cards which is fed the lat and lon data from the original getCity function search
+function getDailyForecast(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=${weatherAPIkey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("Forecast data: ", data);
 
-// dateMainEl.innerHTML = data. moment needed !!!
+      //append to dom
+      weatherBoxSmallEl.innerHTML = "";
 
+      // for loop used to iterate through the days, prevent duplicates of the current day and limit the selection to 5 cards
+      let dailyForecasts = data.daily;
+      for (var i = 1; i < dailyForecasts.length - 2; i++) {
+        createForecastCards(dailyForecasts[i]);
+      }
+    });
+}
 
+// function to append the forecast cards using the for loop in the getDailyForecast function
+function createForecastCards(dailyForecasts) {
+  var dailyIcon = `https://openweathermap.org/img/w/${dailyForecasts.weather[0].icon}.png`;
+  var dailyIconDes = dailyForecasts.weather[0].description;
+  var dailyTime = new Date(dailyForecasts.dt * 1000);
+  var dailyTimeConv = dailyTime.toDateString();
+  var dailyTempData = dailyForecasts.temp.day;
+  var dailyWindData = dailyForecasts.wind_speed;
+  var dailyHumidData = dailyForecasts.humidity;
+  var dailyUvIndexData = dailyForecasts.uvi;
 
+  let dailyCard = document.createElement("div");
+  let dailyCardDesc = document.createElement("h3");
+  let dailyCardIcon = document.createElement("img");
+  let dailyTimeEl = document.createElement("p");
+  let dailytTempEl = document.createElement("p");
+  let dailyWindEl = document.createElement("p");
+  let dailyHumidEl = document.createElement("p");
+  let dailyUvEl = document.createElement("p");
 
-// end of if statament 
+  if (dailyUvIndexData <= 2) {
+    dailyUvEl.setAttribute("class", "uvGreen");
+  } else if (dailyUvIndexData > 2 && dailyUvIndexData <= 5) {
+    dailyUvEl.setAttribute("class", "uvYellow");
+  } else if (dailyUvIndexData > 5 && dailyUvIndexData <= 7) {
+    dailyUvEl.setAttribute("class", "uvOrange");
+  } else {
+    dailyUvEl.setAttribute("class", "uvRed");
+  }
 
-// end of event function
-)
+  dailyCardDesc.textContent = "Forecast:";
+  dailyCardIcon.setAttribute("src", dailyIcon);
+  dailyCardIcon.setAttribute("alt", dailyIconDes);
+  dailyTimeEl.textContent = dailyTimeConv;
+  dailytTempEl.textContent = "Temperature: " + dailyTempData + " Celsius";
+  dailyWindEl.textContent = "Wind Speed: " + dailyWindData + " m/s";
+  dailyHumidEl.textContent = "Humidity: " + dailyHumidData + "%";
+  dailyUvEl.textContent = "UV Index: " + dailyUvIndexData;
 
+  dailyCard.setAttribute("class", "bg-gradient p-2 m-2 rounded");
 
+  dailyCard.append(
+    dailyCardDesc,
+    dailyCardIcon,
+    dailyTimeEl,
+    dailytTempEl,
+    dailyWindEl,
+    dailyHumidEl,
+    dailyUvEl
+  );
+  weatherBoxSmallEl.append(dailyCard);
+}
 
-
-
-// function fetchData() {
-//     var myKey = "840819e30adace1a99c607cc7e47419d"
-    
-//     var cityQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&appid=" + myKey
-    
-//     fetch(cityQuery)
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-    
-// }
-
-// MAKE THE KEY INTO IT'S OWN FUNCTION????
-
-
-// fetch(cityQuery)
-
-// to do
-// unit conversion
-// 5 day forecast
-// pull data from api call
+// event listener on the search button which takes the user input and runs the getCity function to pull API data and run all consecutive functions
+submitBtn.addEventListener("click", getCity);
